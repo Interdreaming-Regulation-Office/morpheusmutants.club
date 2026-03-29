fe1d809c6f0ac240432ea16296b3678bd6ff1ef2
KingMob <john@onethreethree.net>
2026-03-29 14:02:28 +0100

    added more violations

--- web/components/EndAnalysis.tsx
+++ web/components/EndAnalysis.tsx
@@ -22,13 +22,38 @@ export default function EndAnalysis() {
             <li>HydeNSikh</li>
             <li>breamange</li>
             <li>(alias unknown) Site Admin </li>
+            <li>anon678</li>
         </ul>
         <p>
             Category 1 Violations (assisting restricted activity):</p>
-        <p>User: </p>
+        <p>User: SymbioticSemantic1077 aka Percy Spent, birthdate 6/4/2019
+            Action: Assigned 40 Community Reperation Work Units.
+        </p>
+        <p>Users with insufficient vocab identifiability:</p>
+        <ul>
+            <li>Anon240</li>
+            <li>Anon302</li>
+        </ul>
         <p>Category 2 Violations (possession of restricted equipment)</p>
+        <p>User: SpartanSpecialSauce aka Marshall Reed, birthdate 25/9/2027</p>
+        <p>User: user1009 aka Selene Moondragon aka Paula Creek, birthdate 31/3/2017</p>
+        <p>User: REMster aka Taigh O'Rourke, birthdate 7/7/2022</p>
+        <p>Users with insufficient vocab identifiability:</p>
+        <ul>
+            <li>Anon308</li>
+        </ul>
         <p>Category 3 Violations (use of restricted equipment; sale of restricted equipment)</p>
-        <p>Open questions:</p>
+        <p>User: DaDreamDude aka DaDreamDude2 aka NotDaDreamDude aka Steven Reith, birthdate 13/8/2024
+            Action: Apprehended, resonator parts seized. Sentenced to 5 year prison term. Began investigation into supply chain.
+        </p>
+        <p>User: AB aka Dr Alien Brainstorm aka Alan Branston, birthdate 7/5/2009
+            Action: Added evidence to existing investigation into this serial user of restricted equipment.
+        </p>
+        <p>Users with insufficient vocab identifiability:</p>
+        <ul>
+            <li>Anon299</li>
+        </ul>
+        <p>Contribute to the investigation:</p>
         <Question q={"On 8/7/52, was the post signed AB authored by known resonator operator Alan Branston (aka Alien Brainstorm) or an imposter?"} />
         <Question q={"Who was the site admin?"} />
         <Question q={"With the shutdown of Morpheus Mutants Club, where will the next set of category 0 violations come from?"} />
